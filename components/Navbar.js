87b76f988606af4c7d537518a97f388d2c78e671
Rahul <rahulscodingworld@gmail.com>
2023-02-27 11:06:29 +0530

    structural changes

--- components/Navbar.js
+++ components/Navbar.js
@@ -7,14 +7,14 @@ function Navbar({ places, links }) {
 
 
   return (
-    <div className="flex w-full justify-end gap-4 mt-3 text-sm dark:text-slate-100">
+    <div className="justify-items-end mt-10 text-sm dark:text-slate-100">
       {places.map((place, item) => {
         return (
           <div
-            className="underline  underline-offset-2 hover:underline-offset-2 hover:decoration-wavy"
+            className="underline underline-offset-2 hover:underline-offset-2 hover:decoration-wavy mb-4"
             key={place}
           >
-            <Link href={links[item]}>{place}</Link>
+            <Link href={links[item]} className=''>{place}</Link>
           </div>
         );
       })}
--- pages/index.js
+++ pages/index.js
@@ -6,43 +6,51 @@ import Footer from "../components/Footer";
 
 export default function Home() {
   return (
-    <div className="flex flex-col justify-center items-start max-w-2xl sm:mx-auto pb-16 mx-6">
-      <div className="text-2xl mt-20">Rahul Rajkumar</div>
-      <Navbar
-        places={["Projects", "Journal"]}
-        links={["/projects", "/journal"]}
-      />
-      <div className="special dark:text-slate-200 mt-10 leading-relaxed tracking-wide">
-        <p className="pb-4">
-          Hey, I&#39;m a Full Stack Developer, a High School Student, a Ml/AI
-          and Blockchain Enthusiast.
-        </p>
-        <p className="pb-4">
-          I&#39;ve been programming as early as middle school. I first fell into
-          love programming when I started learning about data science with
-          Python. Ever since then I&#39;ve learned a lot more about the space of
-          technology. I learned more language since then like Rust, Javascript,
-          or Solidity.{" "}
-        </p>
-        <p className="pb-4">
-          Currently, I&#39;m going back and learning more data science and ML/AI
-          with Python but still continue to get lost in the space of Web
-          Development.
-        </p>
-        <p className="pb-4">
-          Other than programming, I love travelling, biking, and badminton. If
-          it weren&#39;t for these hobbies, I would be doing programming 24/7.
-          To know a little bit more about me, you can find my{" "}
-          <Link
-            href="/social"
-            className="underline hover:decoration-wavy underline-offset-2"
-          >
-            socials
-          </Link>{" "}
-          here.{" "}
-        </p>
-      </div>
+    <div className="flex flex-col justify-center items-start max-w-3xl sm:mx-auto pb-16 mx-6">
+      <Head>
+        <title>Rahul Rajkumar</title>
+      </Head>
+      <div className="grid grid-cols-8 gap-4">
+        <div>
+          <img src='/r.png' className=' w-10 h-10 mt-32' />
+          <Navbar
+            places={["Projects", "Journal"]}
+            links={["/projects", "/journal"]}
+          />
+        </div>
+        <div className="special dark:text-slate-200 mt-10 leading-relaxed tracking-wide col-span-7">
+          <div className="text-2xl mt-20 mb-10">Rahul Rajkumar</div>
+          <p className="pb-4">
+            Hey, I&#39;m a Full Stack Developer, a High School Student, a Ml/AI
+            and Blockchain Enthusiast.
+          </p>
+          <p className="pb-4">
+            I&#39;ve been programming as early as middle school. I first fell into
+            love programming when I started learning about data science with
+            Python. Ever since then I&#39;ve learned a lot more about the space of
+            technology. I learned more language since then like Rust, Javascript,
+            or Solidity.{" "}
+          </p>
+          <p className="pb-4">
+            Currently, I&#39;m going back and learning more data science and ML/AI
+            with Python but still continue to get lost in the space of Web
+            Development.
+          </p>
+          <p className="pb-4">
+            Other than programming, I love travelling, biking, and badminton. If
+            it weren&#39;t for these hobbies, I would be doing programming 24/7.
+            To know a little bit more about me, you can find my{" "}
+            <Link
+              href="/social"
+              className="underline hover:decoration-wavy underline-offset-2"
+            >
+              socials
+            </Link>{" "}
+            here.{" "}
+          </p>
       <Footer text="Design inspired by Nextra" />
+        </div>
+      </div>
     </div>
   );
 }
