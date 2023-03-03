289d781dd13b94da4bd1dfec5fc4a7a2a1e8b30c
Rahul <rahulscodingworld@gmail.com>
2023-03-03 14:27:18 +0530

    ready for depolyment

--- pages/social.js
+++ pages/social.js
@@ -2,22 +2,26 @@ import React from "react";
 import Head from "next/head";
 import Code from "../components/Code";
 import Logo from '../components/Logo'
+import Navbar from '../components/Navbar'
 import { motion } from "framer-motion";
 
 function Social() {
   return (
-    <div className="flex flex-col justify-center items-start max-w-2xl sm:mx-auto pb-16 mx-6">
+    <div className="flex flex-col justify-center items-start max-w-3xl sm:mx-auto pb-16 mx-6">
       <Head>
         <title>Socials | Rahul Rajkumar</title>
       </Head>
+      <div className="grid lg:grid-cols-8 grid-rows-8 gap-4">
+        <div>
+          <Logo />
+          <Navbar />
+        </div>
+      <div className="special dark:text-slate-200 lg:mt-10 leading-relaxed tracking-wide row-span-7 lg:col-span-7">
       <div className="text-2xl mt-20">Socials</div>
-      
       <div className=" dark:text-slate-200 mt-10 leading-relaxed tracking-wide text-sm">
-        <div className="pb-6">
+        <div className="pb-6 w-full">
           My socials are below, but you can also quickly access all my socials
-          by doing,
-          <Code text="rahulrajkumar.me/{i}" className="inline" />, where i
-          represents the name of the social.
+          by doing, rahulrajkumar.me/&#123;i&#x7D; , where i represents the name of the social.
         </div>
         <motion.div
           whileHover={{ scale: 1.03 }}
@@ -105,8 +109,9 @@ function Social() {
             </div>
           </a>
         </motion.div>
-        
       </div>
+        </div>
+        </div>
     </div>
   );
 }
