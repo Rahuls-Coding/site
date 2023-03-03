f38a49354013a11fbe9827cb273ef7fd87e1f2dd
Rahul <rahulscodingworld@gmail.com>
2023-03-03 14:01:48 +0530

    amazing animations for socials

--- pages/social.js
+++ pages/social.js
@@ -1,10 +1,15 @@
 import React from "react";
 import Link from "next/link";
+import Head from 'next/head';
 import Code from "../components/Code";
+import { motion } from 'framer-motion';
 
 function Social() {
   return (
     <div className="flex flex-col justify-center items-start max-w-2xl sm:mx-auto pb-16 mx-6">
+      <Head>
+        <title>Socials | Rahul Rajkumar</title>
+      </Head>
       <div className="text-2xl mt-20">Socials</div>
       <Link
         className="flex w-full items-center justify-end gap-2 mt-3 text-sm dark:text-slate-100"
@@ -36,15 +41,74 @@ function Social() {
           <Code text="rahulrajkumar.me/{i}" className="inline" />, where i
           represents the name of the social.
         </div>
-        <Link
-          href="https://github.com/rahuls-coding"
-          className="transition ease-in-out duration-700 border-b-2 dark:border-[#1C1C1C] dark:hover:border-[#2e2e2e] border-white hover:border-[#e8e8e8] grid grid-cols-2 content-center py-1 mb-3 w-full text-sm"
-        >
-          <div className="justify-self-start ">Github</div>
-          <div className="justify-self-end text-gray-500 dark:text-gray-400">
-            All of my open-sourced and personal projects.
-          </div>
-        </Link>
+        <motion.div
+           whileHover={{ scale: 1.03 }}
+           whileTap={{ scale: 0.9 }}
+           transition={{ type: "spring", stiffness: 400, damping: 17 }}
+           >
+          <a
+            className="hover:bg-gray-200 rounded p-2 grid grid-cols-2 content-center py-1 mb-3 w-full text-sm"
+            href="https://github.com/rahuls-coding"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <div className="justify-self-start ">Github</div>
+            <div className="justify-self-end text-gray-500 dark:text-gray-400">
+              All of my open-sourced and personal projects.
+            </div>
+            </a>
+          </motion.div>
+        <motion.div
+           whileHover={{ scale: 1.03 }}
+           whileTap={{ scale: 0.9 }}
+           transition={{ type: "spring", stiffness: 400, damping: 17 }}
+           >
+          <a
+            className="hover:bg-gray-200 rounded p-2 grid grid-cols-2 content-center py-1 mb-3 w-full text-sm"
+            href="https://twitter.com/rahuls_coding"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <div className="justify-self-start ">Twitter</div>
+            <div className="justify-self-end text-gray-500 dark:text-gray-400">
+              Tweet random thoughts or amplify others
+            </div>
+            </a>
+          </motion.div>
+        <motion.div
+           whileHover={{ scale: 1.03 }}
+           whileTap={{ scale: 0.9 }}
+           transition={{ type: "spring", stiffness: 400, damping: 17 }}
+           >
+          <a
+            className="hover:bg-gray-200 rounded p-2 grid grid-cols-2 content-center py-1 mb-3 w-full text-sm"
+            href="https://mail.google.com/mail/u/0/?fs=1&tf=cm&source=mailto&to=rahulscoding1@gmail.com"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <div className="justify-self-start ">Email</div>
+            <div className="justify-self-end text-gray-500 dark:text-gray-400">
+            Mostly for business/hiring inquiries or for a &#34;Hi!&#34;
+            </div>
+            </a>
+          </motion.div>
+        <motion.div
+           whileHover={{ scale: 1.03 }}
+           whileTap={{ scale: 0.9 }}
+           transition={{ type: "spring", stiffness: 400, damping: 17 }}
+           >
+          <a
+            className="hover:bg-gray-200 rounded p-2 grid grid-cols-2 content-center py-1 mb-3 w-full text-sm"
+            href="https://linkedin.com/in/rahulscoding"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <div className="justify-self-start ">Linkedin</div>
+            <div className="justify-self-end text-gray-500 dark:text-gray-400">
+            A place to connect with me professionally
+            </div>
+            </a>
+          </motion.div>
         <Link
           href="https://twitter.com/rahuls_coding"
           className="transition ease-in-out duration-700 border-b-2 dark:border-[#1C1C1C] dark:hover:border-[#2e2e2e] border-white hover:border-[#e8e8e8] grid grid-cols-2 content-center py-1 mb-3 w-full text-sm"
