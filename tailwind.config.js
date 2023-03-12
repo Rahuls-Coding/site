74674deb79ed08e68f27f4d57b124f0994acb916
Rahul <rahulscodingworld@gmail.com>
2023-03-12 22:17:38 +0530

    socials changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./pages/**/*.{js,jsx}", "./components/**/*.{js,jsx}"],
+  content: [
+    "./pages/**/*.{js,jsx}",
+    "./components/**/*.{js,jsx}",
+    "./layouts/**/*.{js,jsx}",
+  ],
   theme: {
     extend: {},
   },
